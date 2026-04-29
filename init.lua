fcdbad5f033414d5135de91fc7461115d0caadfb
pedroall <cttpedroall@gmail.com>
2026-04-29 23:37:09 +0000

    Update

--- init.lua
+++ init.lua
@@ -10,53 +10,51 @@ Plug('nvim-tree/nvim-tree.lua')
 Plug('windwp/nvim-autopairs')
 Plug('neovim/nvim-lsp')
 Plug('neovim/nvim-lspconfig')
-Plug('hrsh7th/nvim-cmp')
+Plug('sbdchd/neoformat') 
+Plug('hrsh7th/nvim-cmp', { branch = 'stable' } )
 Plug('hrsh7th/cmp-nvim-lsp')
+Plug('hrsh7th/cmp-buffer')
+Plug('hrsh7th/cmp-path')
+Plug('L3MON4D3/LuaSnip')
+Plug('saadparwaiz1/cmp_luasnip')
+Plug('alvan/vim-closetag')
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-capabilities.textDocument.completion.completionItem.insertTextMode = 1
-capabilities.textDocument.completion.completionItem.resolveSupport = {
-	  properties = { "documentation", "detail", "additionalTextEdits" }
-  }
-  capabilities.textDocument.signatureHelp.signatureInformation.parameterInformation = {
-	    labelOffsetSupport = true
-    }
-    capabilities.textDocument.hover.contentFormat = { "markdown", "plaintext" }
-    capabilities.textDocument.definition.linkSupport = true
-    capabilities.textDocument.references.contextSupport = true
-    capabilities.textDocument.documentHighlight = { dynamicRegistration = true }
-    capabilities.textDocument.codeAction = { dynamicRegistration = true }
-    capabilities.textDocument.codeLens = { dynamicRegistration = true }
-    capabilities.textDocument.documentSymbol = { dynamicRegistration = true }
-    capabilities.textDocument.rename = { dynamicRegistration = true }
-    capabilities.textDocument.formatting = { dynamicRegistration = true }
-    capabilities.textDocument.rangeFormatting = { dynamicRegistration = true }
-    capabilities.textDocument.onTypeFormatting = { dynamicRegistration = true }
-    capabilities.textDocument.foldingRange = { dynamicRegistration = true }
-    capabilities.textDocument.completion.dynamicRegistration = true
-require'lspconfig'.pyright.setup{}
-require'lspconfig'.ts_ls.setup{
-    on_attach = function(client, bufnr)
-        local opts = { noremap = true, silent = true, buffer = bufnr }
-        vim.keymap.set('n', 'K', function()
-            vim.lsp.buf.hover()
-            vim.api.nvim_command('wincmd J')
-        end, opts)
-        vim.keymap.set('i', '<C-h>', vim.lsp.buf.signature_help, opts)
-    end,
-    capabilities = capabilities,
-    flags = {
-        debounce_text_changes = 500
-    }
-}
+vim.o.updatetime = 1000
 
 vim.diagnostic.config({
-    update_in_insert = true
+    virtual_text = true,
+    signs = true,
+    underline = true,
+    update_in_insert = true,
+})
+
+vim.api.nvim_create_autocmd({ "CursorHoldI" }, {
+    callback = function()
+        vim.diagnostic.open_float(nil, { focusable = false })
+    end,
 })
 
 vim.call('plug#end')
+
+local cmp = require('cmp')
+
+cmp.setup({
+	      sources = cmp.config.sources({
+		        	{ name = 'nvim_lsp' },
+				{ name = 'luasnip' },
+			}, {
+				{ name = 'buffer' },
+				{ name = 'path' },
+			})
+								
+})
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+require'lspconfig'.pyright.setup{}
+require'lspconfig'.ts_ls.setup({
+	capabilities = capabilities
+})
 
 o.number = true
 o.tabstop = 4
@@ -72,29 +70,6 @@ require('nvim-autopairs').setup()
 
 k.set('n', '<C-n>', ':new +resize10 term://zsh<CR>', { noremap=true, silent=true })
 k.set('n', '<C-b>', ':NvimTreeToggle<CR>', { noremap=true, silent=true })
-k.set('n', '<C-p>', ':call CocAction(\'runCommand\', \'prettier.formatFile\')<CR>', {noremap=true, silent=true})
-
-local cmp = require('cmp')
-
-cmp.setup({
-    window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered({
-            border = 'single',
-            max_width = 80,
-            max_height = 12,
-            scrollable = true
-        })
-    },
-    mapping = cmp.mapping.preset.insert({
-        ['<Tab>'] = cmp.mapping.confirm({ select = true }),
-        ['<Down>'] = cmp.mapping.select_next_item(),
-        ['<Up>'] = cmp.mapping.select_prev_item(),
-    }),
-    sources = {
-        { name = 'nvim_lsp' },
-        { name = 'buffer' },
-    }
-})
+k.set('n', '<C-p>', ':Neoformat prettier<CR>', { noremap=true, silent=true })
 
 vim.cmd.colorscheme('yugen')
